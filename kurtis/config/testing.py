d40fa49296eaa0403d86dd2c5109d3b2acd65903
Massimo Scamarcia <massimo.scamarcia@gmail.com>
2024-11-17 17:51:04 +0100

    add 360m model config

---
+++ kurtis/config/testing.py
@@ -0,0 +1,7 @@
+from .default import *  # noqa: F403
+
+TRANSFORMERS_MODEL_PRETRAINED = "HuggingFaceTB/SmolLM2-360M-Instruct"
+DATA_AUGMENTATION_MODEL = "HuggingFaceTB/SmolLM2-360M-Instruct"
+INFERENCE_MODEL = "mrs83/Kurtis-SmolLM2-360M-Instruct"
+MODEL_NAME = "Kurtis-SmolLM2-360M-Instruct"
+HF_REPO_ID = "mrs83/Kurtis-SmolLM2-360M-Instruct"
