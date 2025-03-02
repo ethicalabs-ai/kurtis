ec50edae97e3e0b7219e4754537c752f5f6c34f6
Massimo Scamarcia <massimo.scamarcia@gmail.com>
2025-03-02 14:53:54 +0100

    qwen2.5

--- kurtis/config/qwen25_500m.py
+++ kurtis/config/qwen25_500m.py
@@ -4,11 +4,11 @@ from peft import LoraConfig, TaskType
 
 TRANSFORMERS_MODEL_PRETRAINED = "Qwen/Qwen2.5-0.5B-Instruct"
 DATA_AUGMENTATION_MODEL = "HuggingFaceTB/SmolLM2-360M-Instruct"
-INFERENCE_MODEL = "mrs83/Kurtis-Qwen2.5-0.5B-Instruct"
+INFERENCE_MODEL = "ethicalabs/Kurtis-Qwen2.5-0.5B-Instruct"
 MODEL_NAME = "Kurtis-Qwen2.5-0.5B-Instruct"
 MODEL_DPO_NAME = "Kurtis-Qwen2.5-0.5B-Instruct-DPO"
-HF_REPO_ID = "mrs83/Kurtis-Qwen2.5-0.5B-Instruct"
-HF_DPO_REPO_ID = "mrs83/Kurtis-Qwen2.5-0.5B-Instruct-DPO"
+HF_REPO_ID = "ethicalabs/Kurtis-Qwen2.5-0.5B-Instruct"
+HF_DPO_REPO_ID = "ethicalabs/Kurtis-Qwen2.5-0.5B-Instruct-DPO"
 LORA_CONFIG = LoraConfig(
     task_type=TaskType.CAUSAL_LM,
     r=12,
